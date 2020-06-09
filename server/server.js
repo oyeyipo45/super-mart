ccd476f997c9bba0cf838f3642191b9ddf9e584c
oyeyipo45 <oyeyipo45@gmail.com>
2020-06-10 00:38:59 +0100

    server set up

---
+++ server/server.js
@@ -0,0 +1,27 @@
+// const express = require("express");
+// const app = express();
+
+// const data = require('./data/data.js')
+import express from "express";
+import data from "./data/data";
+const app = express();
+
+
+app.get("/api/products", async (req, res, next) => {
+    try {
+        res.send(data.products);
+        console.log("data", data);
+        
+    } catch (error) {
+        next(error)
+    }
+});
+
+
+
+app.listen(5004, () =>{
+    console.log("app started at port 5004");
+    
+})
+
+
