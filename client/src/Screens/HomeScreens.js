77c8ea93ab187bee25bee8c7a5b6b8b05dd645fe
oyeyipo45 <oyeyipo45@gmail.com>
2020-06-10 01:34:10 +0100

    connected client and server

--- client/src/Screens/HomeScreens.js
+++ client/src/Screens/HomeScreens.js
@@ -1,18 +1,35 @@
-import React, { Component } from "react";
-import data from "../data/data";
-import { Link } from "react-router-dom"
-
-
-
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
 
 const HomeScreen = (props) => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const { data } = await axios.get("/api/products");
+      setProducts(data);
+    };
+    fetchData();
+    return () => {
+      //
+    };
+  }, []);
+
   return (
     <ul className="products">
-      {data.products.map((product) => (
+      {products.map((product) => (
         <li key={product._id}>
           <div className="product">
-          <Link to={`/product/${product._id}`}> <img className="product-image" src={product.image} alt="products" /></Link>
-           
+            <Link to={`/product/${product._id}`}>
+              {" "}
+              <img
+                className="product-image"
+                src={product.image}
+                alt="products"
+              />
+            </Link>
+
             <div className="product-name">
               <Link to={`/product/${product._id}`}>{product.name}</Link>
             </div>
