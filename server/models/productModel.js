cc84fd5945deb4de9ed22e9100745c00aa00e4b2
oyeyipo45 <oyeyipo45@gmail.com>
2020-07-23 02:52:11 +0100

    Product schema Fixed

--- server/models/productModel.js
+++ server/models/productModel.js
@@ -1,27 +1,7 @@
 import mongoose from "mongoose";
 
-<<<<<<< HEAD
-
-const productSchema = new mongoose.Schema({
-    name: {type: String, required: true},
-    category: {type: String, required: true,},
-    image: {type: String, required: true},
-    price: {type: String, required: true},
-    brand: {type: String, required: true,},
-    rating: {type: String, required: true},
-    numReviews: {type: String, required: true},
-    countInStock: {type: String, required: true}
-
-    
-
-});
 
 
-
-const productModel = mongoose.model(`products`, productSchema)
-
-export default productModel;
-=======
 const productSchema = new mongoose.Schema({
   name: { type: String, required: true },
   category: { type: String, required: true },
@@ -36,4 +16,3 @@ const productSchema = new mongoose.Schema({
 const productModel = mongoose.model("products", productSchema);
 
 export default productModel;
->>>>>>> 5f1c4562987e65f84c5388460663a46ba72fd013
--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -1,6 +1,5 @@
 import express from "express";
 import Product from '../models/productModel';
-import Product from "../models/productModel"
 
 
 const router = express.Router();
