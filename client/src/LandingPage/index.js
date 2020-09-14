4223f8479a1ab662f714e3e950fbe5b6a6a4df25
oyeyipo45 <oyeyipo45@gmail.com>
2020-09-14 11:23:30 +0100

    changed naming for landing page

--- client/src/LandingPage/index.js
+++ client/src/LandingPage/index.js
@@ -1,8 +1,8 @@
 import React from 'react'
-import Footer from "./footer";
-import Header from './header';
+import Footer from "./Footer";
+import Header from './Header';
 // import SideBar from "./sideBar";
-import Body from "./body";
+import Body from "./Body";
 
 
 
